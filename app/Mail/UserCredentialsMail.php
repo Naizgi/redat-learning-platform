d383d947f1a2b56161b26755bf98390cc84c0a81
naizgi hayilekiros <163419887+Naizgi@users.noreply.github.com>
2026-01-22 00:02:56 +0300

    Update UserCredentialsMail.php

---
+++ app/Mail/UserCredentialsMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UserCredentialsMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        $subject = match($this->data['type'] ?? 'new') {
+            'new' => 'Welcome to ' . ($this->data['siteName'] ?? 'Redat Learning Hub'),
+            'update' => 'Your Account Information Has Been Updated',
+            'reset' => 'Your Password Has Been Reset',
+            'reminder' => 'Your Account Credentials',
+            default => 'Your Account Information'
+        };
+            
+        return new Envelope(
+            subject: $subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.user-credentials',
+            with: $this->data
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
